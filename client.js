9a13b0321429dd4fc3f275e95f6675e54a58d7d1
lifund <jan9won@gmail.com>
2020-11-13 14:54:54 +0900

    temp page

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 const port = 3000;
 
-app.listen(port, ()=>{console.log('client listening port:', port)});
+app.listen(port, ()=>{console.log('seomin client listening port:', port)});
 app.set('trust proxy', true);
 app.use(express.json({ limit: '5mb'}));
 app.use('/public', express.static('public'));
@@ -12,5 +13,5 @@ app.use(bodyParser.json());
   
 
 app.get('/',(req,res)=>{
-	res.send('서민피자.com 정상적으로 설치되었습니다.<br> 홈페이지 준비중입니다.');
+	res.sendFile(path.join(__dirname,'public/temp.html'));
 })
