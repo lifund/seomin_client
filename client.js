2c60bb7bd4858f85bb6dcf1696965259c0d7faf8
lifund <jan9won@gmail.com>
2020-11-27 16:43:39 +0900

    css

--- client.js
+++ client.js
@@ -34,7 +34,7 @@ app.get('/robots.txt',(req,res)=>{
 	res.sendFile(path.join(__dirname,'public/robots.txt'));
 })
 app.get('/',(req,res)=>{
-	res.redirect('/temp');
+	res.redirect('/home');
 });
 app.get('/temp',(req,res)=>{
 	res.sendFile(path.join(__dirname,'public/temp.html'));
