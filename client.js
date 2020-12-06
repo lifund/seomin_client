2de1a0d17ca5860d92163e68f4edbb6ad6db89ea
lifund <jan9won@gmail.com>
2020-12-06 16:53:39 +0900

    admin mobile style : button color

--- client.js
+++ client.js
@@ -46,7 +46,7 @@ app.get('/naver1d6c556ca6b4a04b8ca411598a563b03.html',(req,res)=>{
 	res.sendFile(path.join(__dirname,'public/naver1d6c556ca6b4a04b8ca411598a563b03.html'));
 });
 app.get('/sitemap',(req,res)=>{
-	res.sendFile(path.join(__dirname,'public/sitemap.xml'));
+	res.sendFile(path.join(__dirname,'/public/sitemap.xml'));
 });
 
 
